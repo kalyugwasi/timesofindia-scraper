96da162e534dd22487edc25980e3c39b215dba3d
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-02-14 00:40:33 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -21,7 +21,7 @@ OUTPUT_DIR = "news_articles"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
-# ===============================
+# =============================
 
 class FastNewsScraper:
     def __init__(self):
